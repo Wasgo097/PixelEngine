85b4c0c26e6e24c302c007b1f9db97328b017b61
Wasgo097 <patrykpakula321@gmail.com>
2021-10-09 10:04:48 +0200

    actorsetting json serialziable/deserializable deserializable return's bool

--- PixelEngine/Headers/Settings/ActorSettings.h
+++ PixelEngine/Headers/Settings/ActorSettings.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "SettingsBase.h"
 #include "TextureSettings.h"
+#include "Utility/ISerializable.h"
 namespace Settings{
 	enum class CollisionType{
 		None,
@@ -11,17 +12,53 @@ namespace Settings{
 		Static,
 		Dynamic
 	};
-	class ActorSettings :public SettingsBase{
+	class ActorSettings :public SettingsBase,Utility::IJsonSerializable{
 	public:
 		ActorSettings(CollisionType collision,ActorType type, sf::Vector2f position,sf::Vector2f collidersize, sf::Vector2f velocity,bool tickflag, BaseTextureSettings texturesettings):
 			_collision(collision),_type(type),_position(position),_collidersize(collidersize),_velocity(velocity),_tickflag(tickflag),_texture(texturesettings){}
 		ActorSettings() = default;
-		// Inherited via SettingsBase
 		virtual std::string ToStdString()const override{
 			std::stringstream stream;
-			stream<< "Collision Type: " << static_cast<int>(_collision) << " Actor Type: " << static_cast<int>(_type) << "Location "<<_position.x<<" "<<_position.y<<
-				" Collider Size: " << _collidersize.x << " " <<_collidersize.y <<" Velocity: "<<_velocity.x<<" "<<_velocity.y<< " Tick Flag: " <<_tickflag;
+			stream << "Collision Type: " << static_cast<int>(_collision) << " Actor Type: " << static_cast<int>(_type) << "Location " << _position.x << " " << _position.y <<
+				" Collider Size: " << _collidersize.x << " " << _collidersize.y << " Velocity: " << _velocity.x << " " << _velocity.y << " Tick Flag: " << _tickflag;
 			return stream.str();
+		}
+		virtual json JSerialize()const override{
+			try{
+				json output;
+				output["_collision"] = static_cast<int>(_collision);
+				output["_type"] = static_cast<int>(_type);
+				output["_positionx"] = _position.x;
+				output["_positiony"] = _position.y;
+				output["_collidersizex"] = _collidersize.x;
+				output["_collidersizey"] = _collidersize.y;
+				output["_velocityx"] = _velocity.x;
+				output["_velocityy"] = _velocity.y;
+				output["_tickflag"] = _tickflag;
+				//todo texture settings
+				return output;
+			}
+			catch(...){
+				return json();
+			}
+		}
+		virtual bool JDeserialize(json json)override{
+			try{
+				_collision = static_cast<CollisionType>(json["_collision"]);
+				_type = static_cast<ActorType>(json["_type"]);
+				_position.x = json["_positionx"];
+				_position.y = json["_positiony"];
+				_collidersize.x = json["_collidersizex"];
+				_collidersize.y = json["_collidersizey"];
+				_velocity.x = json["_velocityx"];
+				_velocity.y = json["_velocityy"];
+				_tickflag = json["_tickflag"];
+				//todo texture settings
+				return true;
+			}
+			catch(...){
+				return false;
+			}
 		}
 		const CollisionType& GetCollision()const{
 			return _collision;
--- PixelEngine/Headers/Utility/ISerializable.h
+++ PixelEngine/Headers/Utility/ISerializable.h
@@ -7,19 +7,19 @@ namespace Utility{
 	class IDiskSerializable{
 	public:
 		virtual void DiskSerialize(const std::string & path)const = 0;
-		virtual void DiskDeserialize(const std::string& path) = 0;
+		virtual bool DiskDeserialize(const std::string& path) = 0;
 	};
 	class IStringSerializable{
 	public:
 		virtual std::string StrSerialize()const = 0;
-		virtual void StrDeserialize(std::string) = 0;
+		virtual bool StrDeserialize(std::string) = 0;
 	};
 	class IStringsArraySerializable{
 		virtual std::vector<std::string> ArrSerialize()const = 0;
-		virtual void ArrDeserialize(std::vector<std::string>) = 0;
+		virtual bool ArrDeserialize(std::vector<std::string>) = 0;
 	};
 	class IJsonSerializable{
 		virtual json JSerialize()const = 0;
-		virtual void JDeserialize(json json) = 0;
+		virtual bool JDeserialize(json json) = 0;
 	};
 }

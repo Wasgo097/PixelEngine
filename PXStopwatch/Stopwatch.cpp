45a2e1ca3b5e7ca6872e06e6465388602f33f27a
Wasgo097 <patrykpakula321@gmail.com>
2022-09-14 16:42:09 +0200

    Changed unit in stopwatch

--- PXStopwatch/Stopwatch.cpp
+++ PXStopwatch/Stopwatch.cpp
@@ -8,5 +8,5 @@ void Stopwatch::Reset() {
 	start_time = std::chrono::high_resolution_clock::now();
 }
 std::uint64_t Stopwatch::Elapsed() {
-	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
+	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start_time).count();
 }

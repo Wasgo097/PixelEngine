157d93edf66a431ed0bf961fad8741f013a201bb
Wasgo097 <patrykpakula321@gmail.com>
2022-10-01 16:41:06 +0200

    Fixed music playing

--- PXSound/MusicManager.cpp
+++ PXSound/MusicManager.cpp
@@ -5,15 +5,15 @@ namespace Sound {
 	}
 	void MusicManager::SetVolume(float new_volume) const {
 		if (_current_playing_music)
-			_current_playing_music->setVolume(new_volume);
+			_current_playing_music->setVolume(100.0*new_volume);
 	}
 	void MusicManager::PlayMusic(std::unique_ptr<sf::Music>&& new_music) {
 		if (_current_playing_music)
 			_current_playing_music->pause();
 		_current_playing_music = std::move(new_music);
-		_current_playing_music->play();
 		_current_playing_music->setLoop(true);
 		SetVolume(_settings.master_vol * _settings.music_vol);
+		_current_playing_music->play();
 	}
 	void MusicManager::StopMusic()const {
 		_current_playing_music->stop();
